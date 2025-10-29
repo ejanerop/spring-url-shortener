d89c5b2b4f215564d6a58e11917d4710bbefa3c6
Eric Janero <eric.janero.ext@idrica.com>
2025-10-29 19:00:31 +0100

    feat: init repo

---
+++ src/main/java/com/janero/spring_url_shortener/SpringUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.janero.spring_url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUrlShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janero/spring_url_shortener/SpringUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janero.spring_url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
