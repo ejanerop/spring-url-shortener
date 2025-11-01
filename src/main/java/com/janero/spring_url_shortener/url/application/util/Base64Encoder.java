fafbc18389880820e2cfcd2e09fbbe943e735b43
Eric Janero <eric.janero.ext@idrica.com>
2025-11-01 14:25:40 +0100

    feat: add random salt to key generation

--- src/main/java/com/janero/spring_url_shortener/url/application/util/Base64Encoder.java
+++ src/main/java/com/janero/spring_url_shortener/url/application/util/Base64Encoder.java
@@ -4,11 +4,14 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.Random;
 import com.janero.spring_url_shortener.shared.domain.exception.EncodeException;
 
 public class Base64Encoder {
 
     private Base64Encoder() {}
+
+    private static Random random = new Random();
 
     public static String encode(String url) throws EncodeException {
         try {
@@ -17,11 +20,19 @@ public class Base64Encoder {
 
             String base64 = Base64.getUrlEncoder().withoutPadding().encodeToString(hashBytes);
 
-            return base64.substring(0, 8);
+            String hash = base64.substring(0, 8);
+            String salt = generateRandomSalt();
+            return hash.concat(salt);
 
         } catch (NoSuchAlgorithmException e) {
             throw new EncodeException("Error generating hash", e);
         }
+    }
+
+    private static String generateRandomSalt() {
+        byte[] saltBytes = new byte[6];
+        random.nextBytes(saltBytes);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(saltBytes);
     }
 
 }
